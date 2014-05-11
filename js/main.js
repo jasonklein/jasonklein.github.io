ca7855941e71d0cf13a42cf1d061e3398f2ff1dd
jasonklein <jasonklein@gmail.com>
2014-05-11 14:51:20 +0100

    Add SVGs for icons, embed twitter feed and add javascript to reveal/show twitter feed.

--- js/main.js
+++ js/main.js
@@ -2,14 +2,21 @@ var PersonalSite = PersonalSite || {};
 
 
 PersonalSite.showContent = function(content) {
-  content.slideDown();
   content.data('state', 'visible');
-}
+  content.slideDown();
+  if ($("#twitter").data('state') === 'visible'){
+    $("#twitter-content").slideDown(800);
+  }
+};
 
 PersonalSite.hideContent = function(content) {
-  content.slideUp();
   content.data('state', 'hidden');
-}
+  content.slideUp();
+  $("#twitter-content").slideUp();
+  if ($("#twitter").data('state') === 'hidden'){
+    $("#twitter-content").slideUp(800);
+  }
+};
 
 PersonalSite.toggleContent = function() {
   var content = $(this).find(".icon-name");
@@ -22,8 +29,10 @@ PersonalSite.toggleContent = function() {
 
 PersonalSite.setup = function() {
   $(".icon-name").data('state', 'hidden').hide();
+  $("#twitter-content").hide();
   $(".icon-box").click(PersonalSite.toggleContent);
 };
+
 
 
 $(PersonalSite.setup);
